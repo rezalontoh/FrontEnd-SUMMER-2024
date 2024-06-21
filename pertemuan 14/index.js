fc34f98fa52f8f406e56d4715a7b96281b9e3c94
Lontoh, Reza Michael Yohanes <158019671+rezalontoh@users.noreply.github.com>
2024-06-21 07:37:40 +0700

    Update index.js

--- pertemuan 14/index.js
+++ pertemuan 14/index.js
@@ -1,3 +1,6 @@
 //working on remote repository
 console.log("Hello Git and Github");
 console.log("Coba Lagi");
+console.log("Ini diubah langsung di github");
+console.log("Test git pull");
+console.log("Test merge conflic");
