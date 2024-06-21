ff8c950b97b20692f4b5cb9cc968db6b53c34bf0
rezalontoh <s2200063@student.unklab.ac.id>
2024-06-21 07:47:33 +0700

    update index.js testiing merge conflic

--- pertemuan 14/index.js
+++ pertemuan 14/index.js
@@ -3,4 +3,6 @@ console.log("Hello Git and Github");
 console.log("Coba Lagi");
 console.log("Ini diubah langsung di github");
 console.log("Test git pull");
+//test
 console.log("Test merge conflic");
+
